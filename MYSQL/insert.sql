6e08fa47413e765bd02dc3e2e8934bfb4a00a0e2
Susann-18 <151371561+Susann-18@users.noreply.github.com>
2023-12-14 17:22:16 +0530

    Add files via upload

---
+++ MYSQL/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO Students(StudentsID,LastName,FirstName,City,Mark)
+values(1,'Anu','suja','chengannur',95),
+(2,'Thomas','Anu','kollam',100),
+(3,'Abhram','pretty','Adoor',95)
+;
---
+++ MYSQL/students.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Students
+(StudentsID  int,
+LastName Varchar(255),
+FirstName Varchar(255),
+Address Varchar(255),
+City Varchar(255),
+Mark int
+);
