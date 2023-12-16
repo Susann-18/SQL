3b9208f4a2aad25a2cd8118b5c04a60b6bddf9ee
Susann-18 <151371561+Susann-18@users.noreply.github.com>
2023-12-16 16:12:43 +0530

    Add files via upload

---
+++ MYSQL/saleman.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Saleman
+(Salesman_id int,
+name Varchar(255),
+ city Varchar(255),
+ commission int);
+ 
+ 
+ INSERT INTO Saleman(Salesman_id,name,city,commission)
+VALUES(5001,'James Hoog', 'New York',0.15),
+(5002,'Nail Knite','Paris', 0.13),
+(5005,'Pit Alex','London', 0.11),
+(5006,'Mc Lyon','Paris', 0.14),
+(5003,'Lauson Hen','UK', 0.12),
+(5007,'Paul','Adam Rome',0.13);
